3f37be24acc83e2fd83240a35f30ff0d2a2d6030
acatasic <96747833+acatasic@users.noreply.github.com>
2022-03-16 03:43:33 +0100

    Update film.js

--- LiveServer/film.js
+++ LiveServer/film.js
@@ -6,19 +6,36 @@ export class Film {
         this.brojOcena = brojOcena;
         this.miniKontejner = null;
         this.miniKontejner1 = null;
+        this.miniKontejner0= null;
        
     }
     crtajFilm(host) {
-        this.miniKontejner = document.createElement("div");
-        this.miniKontejner.className = "lok";
-
-        this.miniKontejner1 = document.createElement("div");
-        this.miniKontejner1.className = "popuna";
-        this.miniKontejner1.innerHTML = " "+ this.naziv +" ocena "+ this.ocena;
         
-        this.miniKontejner1.style.flexGrow = this.ocena / 10;
-        this.miniKontejner.appendChild(this.miniKontejner1);
+        this.miniKontejner0=document.createElement("div");
+        this.miniKontejner0.className="glavniKontejnerZaPrikazFilma"
 
-        host.appendChild(this.miniKontejner);
+        var labelaZaOcenu = document.createElement("label");
+        labelaZaOcenu.className= "labelaZaOcenu";
+        labelaZaOcenu.innerHTML=this.ocena;
+        this.miniKontejner0.appendChild(labelaZaOcenu);
+
+            this.miniKontejner = document.createElement("div");
+            this.miniKontejner.className = "glavniKontejnerZaPrikazFilma1";
+
+        this.miniKontejner0.appendChild(this.miniKontejner);
+
+                this.miniKontejner1 = document.createElement("div");
+                this.miniKontejner1.className = "popuna";
+        
+                this.miniKontejner1.style.flexGrow = this.ocena / 10;
+            this.miniKontejner.appendChild(this.miniKontejner1);
+        
+        var labelaZaNaziv = document.createElement("label");
+        labelaZaNaziv.className= "labelaZaNaziv";
+        labelaZaNaziv.innerHTML=this.naziv;
+        this.miniKontejner0.appendChild(labelaZaNaziv)
+
+
+        host.appendChild(this.miniKontejner0);
     }
 }
